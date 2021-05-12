8fadb71b34cc86a28f05ee2a55c7afa99c4caf2c
acalvom <andreac.3108@gmail.com>
2021-05-12 12:25:29 +0200

    UseEffect (in progress)

--- src/components/Personal.js
+++ src/components/Personal.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const Personal = () => {
+
+    /*
+     * useEffect es un hook de React que permite le permite a un componente hacer algo una
+     * vez se ha renderizado, es decir, que tras pintarse el componente, se ejecuta una acción
+     * Es fundamental para consumir datos de un API. En este ejemplo: https://jsonplaceholder.typicode.com/users
+     */
+
+    useEffect(() => {
+        document.title = 'Personal Title'
+    })
+
     return (
         <div>
             <h4> Personal Page </h4>
