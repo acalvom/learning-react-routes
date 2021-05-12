83f26a617223722eda0ee6e600329e7a68674b10
acalvom <andreac.3108@gmail.com>
2021-05-12 11:51:48 +0200

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 function App() {
     return (
-        <div className="container">
-            Start the project
-        </div>
+        <Router>
+            <div className="container">
+                <nav className="navbar navbar-expand-sm bg-light">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Inicio</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Personal</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Usuarios</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </Router>
     );
 }
 
